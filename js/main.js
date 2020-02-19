77fad0ad437dae99b6b312346f42e269620b03be
Wasif <wasefahmed.dyd@gmail.com>
2020-02-20 02:56:48 +0600

    lanta_service

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(function ($) {
+"use strict";
+
+$('.owl-carousel.lanta').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+
+
+
+
+})(jQuery);
